3b05420a5d8bc491f1f029617ee236cb91930925
praveenteja078 <praveen.teja078@gmail.com>
2024-04-14 03:24:45 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className="bgContainer">
+    <h1 className="heading">Super Over League</h1>
+    <div className="imageContainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="teamLogo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="teamLogo"
+      />
+    </div>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
